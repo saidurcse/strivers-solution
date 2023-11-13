0b2f576d29236a1fa27adae65ddd98aba753fd10
Muhammad Saidur Rahman <saidurdev16@gmail.com>
2023-11-13 15:05:08 -0600

    new changes

--- 1_4_coding_ninja_divisors.kt
+++ 1_4_coding_ninja_divisors.kt
@@ -19,3 +19,33 @@ fun main() {
 
 Time Complexity: O(n)
 Space Complexity: O(n) as the ArrayList will store all these values.
+
+
+Better Solution:
+
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.sqrt
+
+fun main() {
+    var sc = Scanner(System.`in`)
+    var n = sc.nextInt()
+
+    var list = ArrayList<Int>()
+
+    for (i in 1 .. sqrt(n.toDouble()).toInt()) {
+        if(n % i == 0) {
+            list.add(i)
+
+            if((n/i) != i)
+                list.add(n/i)
+        }
+    }
+
+    list.sort()
+
+    print(list)
+}
+
+Time Complexity: O(sqrt(n)), because every time the loop runs only sqrt(n) times.
+Space Complexity: O(1), we are not using any extra space.
