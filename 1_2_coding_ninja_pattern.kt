7dd01fa4674679d9add9ef17ccb7367f8072e5c6
Muhammad Saidur Rahman <saidurdev16@gmail.com>
2023-11-24 12:23:11 -0600

    new changes

--- 1_2_coding_ninja_pattern.kt
+++ 1_2_coding_ninja_pattern.kt
@@ -3,11 +3,24 @@ import java.util.*
 fun main() {
     var sc = Scanner(System.`in`)
     var n = sc.nextInt()
-    //var row = sc.nextInt()
-    //var col = sc.nextInt()
 
-    printPattern1(n)    
+    //printPattern1(n)
+    //printPattern2(n)
+    //printPattern3(n)
+    //printPattern4(n)
+    //printPattern5(n)
+    //printPattern6(n)
+    //printPattern7(n)
 }
+/*
+From 1 to 6
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+
+For 7
+Time Complexity: O(n^2)
+Space Complexity: O(n^2)
+*/
 
 fun printPattern1(n: Int) {
     for(i in 0 until n) {
@@ -18,5 +31,55 @@ fun printPattern1(n: Int) {
     }
 }
 
-Time Complexity: O(n^2)
-Space Complexity: O(1)
+fun printPattern2(n: Int) {
+    for(i in 0 until n) {
+        for (j in 0 .. i) {
+            print("* ")
+        }
+        println()
+    }
+}
+
+fun printPattern3(n: Int) {
+    for(i in 1 .. n) {
+        for (j in 1 .. i) {
+            //print("${(j+1)} ")
+            print("$j ")
+        }
+        println()
+    }
+}
+
+fun printPattern5(n: Int) {
+    for(i in 0 until n) {
+        for (j in n downTo  i) {
+            print("* ")
+        }
+        println()
+    }
+}
+
+fun printPattern6(n: Int) {
+    for(i in 1 .. n) {
+        for (j in n downTo   i) {
+            print("${n-j+1} ")
+        }
+        println()
+    }
+}
+
+fun printPattern7(n: Int) {
+    for(i in 0 until  n) {
+        for (j in 0 until n-i-1) { // For printing the spaces
+            print(" ")
+        }
+        for (j in 0 until   2*i+1) { // For printing the stars
+            print("*")
+        }
+        for (j in 0 until   n-i-1) { // For printing the spaces
+            print(" ")
+        }
+        println()
+    }
+}
+
