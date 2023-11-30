de65467291971b53de06bd64e8604a2a69fbc590
Muhammad Saidur Rahman <saidurdev16@gmail.com>
2023-11-30 13:33:40 -0600

    new changes

--- 1_2_coding_ninja_pattern.kt
+++ 1_2_coding_ninja_pattern.kt
@@ -11,15 +11,15 @@ fun main() {
     //printPattern5(n)
     //printPattern6(n)
     //printPattern7(n)
+    //printPattern8(n)
+    //printPattern9(n)
+    //printPattern10(n)
+    printPattern11(n)
+
 }
 /*
-From 1 to 6
 Time Complexity: O(n^2)
 Space Complexity: O(1)
-
-For 7
-Time Complexity: O(n^2)
-Space Complexity: O(n^2)
 */
 
 fun printPattern1(n: Int) {
@@ -78,6 +78,81 @@ fun printPattern7(n: Int) {
         }
         for (j in 0 until   n-i-1) { // For printing the spaces
             print(" ")
+        }
+        println()
+    }
+}
+
+fun printPattern8(n: Int) {
+    for(i in 0 until n) {
+        for (j in 0 until i) { // For printing the spaces
+            print(" ")
+        }
+        for (j in 0 until   (2*n)-(2*i+1)) { // For printing the stars
+            print("*")
+        }
+        for (j in 0 until i) { // For printing the spaces
+            print(" ")
+        }
+        println()
+    }
+}
+
+fun printPattern9(n: Int) {
+    for(i in 0 until  n) {
+        for (j in 0 until n-i-1) { // For printing the spaces
+            print(" ")
+        }
+        for (j in 0 until   2*i+1) { // For printing the stars
+            print("*")
+        }
+        for (j in 0 until   n-i-1) { // For printing the spaces
+            print(" ")
+        }
+        println()
+    }
+
+    for(i in 0 until n) {
+        for (j in 0 until i) { // For printing the spaces
+            print(" ")
+        }
+        for (j in 0 until   (2*n)-(2*i+1)) { // For printing the stars
+            print("*")
+        }
+        for (j in 0 until i) { // For printing the spaces
+            print(" ")
+        }
+        println()
+    }
+}
+
+fun printPattern10(n: Int) {
+
+    for (i in 1 .. 2*n-1) {
+
+        var stars = i
+        if(i > n) stars = 2*n-i
+        for (j in 1 .. stars) {
+            print("*")
+        }
+        print(" ")
+        println()
+    }
+}
+
+fun printPattern11(n: Int) {
+
+    var start = 1
+    for (i in 0 until n) {
+        //flipping
+        if(i%2 == 0)
+            start = 1
+        else
+            start = 0
+
+        for (j in 0 .. i) {
+            print(start)
+            start = 1 - start //flipping
         }
         println()
     }
