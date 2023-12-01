fa6ef15bb4262c9ce8cd22d4d331ea948707455e
Muhammad Saidur Rahman <saidurdev16@gmail.com>
2023-12-01 12:37:47 -0600

    new changes

--- 1_2_coding_ninja_pattern.kt
+++ 1_2_coding_ninja_pattern.kt
@@ -15,7 +15,12 @@ fun main() {
     //printPattern9(n)
     //printPattern10(n)
     //printPattern11(n)
-    printPattern12(n)
+    //printPattern12(n)
+    //printPattern13(n)
+    //printPattern14(n)
+    //printPattern15(n)
+    //printPattern16(n)
+    printPattern17(n)
 
 }
 /*
@@ -177,3 +182,68 @@ fun printPattern12(n: Int) {
     }
 }
 
+fun printPattern13(n: Int) {
+    var number = 1
+    for(i in 1 .. n) {
+        for(j in 1 .. i) {
+           print("${number++} ")
+        }
+        println()
+    }
+}
+
+fun printPattern14(n: Int) {
+    /*One way*/
+    var num: Int
+    for(i in 1 .. n) {
+        num = 65
+        for(j in 1 .. i) {
+            print((num++).toChar())
+        }
+        println()
+    }
+    /*Alternative way*/
+    for(i in 1 .. n) {
+        for(j in 'A' ..'A'+i) {
+            print(j + " ")
+        }
+        println()
+    }
+}
+
+fun printPattern15(n: Int) {
+    for(i in n downTo  0) {
+        for(j in 'A' .. 'A'+i) {
+            print(j + " ")
+        }
+        println()
+    }
+}
+
+fun printPattern16(n: Int) {
+    for(i in 0 until n) {
+        for(j in 0 ..  i) {
+            print(('A'.code + i).toChar() + " ")
+        }
+        println()
+    }
+}
+
+fun printPattern17(n: Int) {
+    for(i in 0 until  n) {
+        for(j in 0 ..  (n-i-1)) {
+            print(" ")
+        }
+        var ch = 'A'
+        var breakPoint = (2*i+1)/2
+        for(j in 1 ..  2*i+1) {
+            print(ch)
+            if(j <= breakPoint) ch++
+            else ch--
+        }
+        for(j in 0 until   (n-i-1)) {
+            print(" ")
+        }
+        println()
+    }
+}
